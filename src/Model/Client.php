cd52331a51eabdd93172f99510974d59436a4f48
Onemore <onemore04@mail.ru>
2020-03-26 10:01:17 +0700

    composer mv to the root

---
+++ src/Model/Client.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Model;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+/**
+ * Class Client
+ * @package Model
+ */
+final class Client extends GuzzleClient
+{
+    //todo
+}
---
+++ src/Model/Message.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Model;
+
+/**
+ * Class Message
+ * @package Model
+ */
+class Message
+{
+    /** @var int $id */
+    private int $id;
+    /** @var string $name */
+    private string $name;
+    /** @var string $text */
+    private string $text;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText(): string
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+}
