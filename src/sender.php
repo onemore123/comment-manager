1460372637c5c5ca39b05a53488513cad31cc5f0
Onemore <onemore04@mail.ru>
2020-03-30 08:29:38 +0700

    template added

--- src/sender.php
+++ src/sender.php
@@ -1,7 +1,8 @@
 <?php
 
+use GuzzleHttp\Psr7\Request;
 use Model\Client;
-use Model\Message;
+use Psr\Http\Message\ResponseInterface;
 
 require 'vendor/autoload.php';
 
@@ -35,22 +36,79 @@ final class Sender
      * @param string $comment
      * @return array
      */
-    public function sendComment(string $author, string $comment): array
+    public function postComment(string $author, string $comment): array
     {
         // Send new comment
         $response = $this->client->request('POST','/comment', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
             'json' => [
                 'name' => $author,
                 'text' => $comment,
             ]
         ]);
 
+        return $this->getDecodedResponse($response);
+    }
+
+    /**
+     * @param string|null $author
+     * @return array
+     */
+    public function getComments(string $author = null): array
+    {
+        // Set up request parameters
+        $headers = ['Accept' => 'application/json'];
+        if ($author !== null) {
+            $query = ['name' => $author];
+        }
+
+        $request = new Request('GET', '/comments', [
+            'headers' => $headers,
+            'query' => isset($query) ? $query : '',
+        ]);
+
+        $response = $this->client->send($request);
+
+        return $this->getDecodedResponse($response);
+    }
+
+    /**
+     * @param int $id
+     * @param string $comment
+     * @return array
+     */
+    public function updateComment(int $id, string $comment)
+    {
+        //TODO: authenticate
+        $url = '/comment/' . $id;
+        $request = new Request('PUT', $url, [
+            'headers' => [
+                'Content-Type' => 'application/json',
+            ],
+            'json' => [
+                'text' => $comment,
+            ]
+        ]);
+
+        $response = $this->client->send($request);
+
+        return $this->getDecodedResponse($response);
+    }
+
+    /**
+     * @param ResponseInterface $response
+     * @return array
+     */
+    private function getDecodedResponse(ResponseInterface $response): array
+    {
         // Check status code
         if ($response->getStatusCode() !== 200) {
             throw new RuntimeException($response->getStatusCode(), $response->getReasonPhrase());
         }
 
-        // Get response body as array
-        $responseBody = json_decode($response->getBody(), true);
+        // Get response body as array to get the comment id
+        return json_decode($response->getBody(), true);
     }
 }
